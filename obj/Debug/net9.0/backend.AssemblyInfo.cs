28fa95f25ef4ba75acf89a2329f5bca1c07220e7
Édua <edua.kaszas@gmail.com>
2025-01-13 22:08:26 +0100

    feat(API): added delete endpoint

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e2369b277eaa2f8ecf98d0c59d2210929bcc4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08be4a90792560a6e6e260581c8cb75c93b631f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
